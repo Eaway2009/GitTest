ffa5142d367547e27fc5434de1e8391174760bac
shenyiwei <shenyiwei@xiaoniu66.com>
2019-04-26 15:04:53 +0800

    增加伪代码

--- Test.java
+++ Test.java
@@ -2,10 +2,6 @@ package com.eaway.test;
 
 public class MainTest {
 	public void test(){
-<<<<<<< 869cd39ec787a4c2b474e49b5f096703451877c1:Test.java
-		log.i("eaway", "test");
-=======
-		log.i("test", "a");
->>>>>>> ad:Test.Java
+		log.i("eaway", "我是测试伪代码");
 	}
 }
