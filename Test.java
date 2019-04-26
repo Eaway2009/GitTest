930632724108ba22d3b143e282b5029d84296883
shenyiwei <shenyiwei@xiaoniu66.com>
2019-04-26 15:01:10 +0800

    ad

--- Test.java
+++ Test.java
@@ -2,6 +2,10 @@ package com.eaway.test;
 
 public class MainTest {
 	public void test(){
+<<<<<<< 869cd39ec787a4c2b474e49b5f096703451877c1:Test.java
 		log.i("eaway", "test");
+=======
+		log.i("test", "a");
+>>>>>>> ad:Test.Java
 	}
 }
